66b1801f3aa7d64757ef5d188d5b02f47d36179b
Yifei Zhang <zhayife@amazon.com>
2020-10-22 21:32:50 +0800

    feat(initial): init project stucture

---
+++ env/SketchBoard.py
@@ -0,0 +1,14 @@
+
+
+class SketchBoardEnv(object):
+    def __init__(self):
+        '''
+        Constructor of SketchBoard.
+        '''
+        pass
+
+    def step(action: list) -> tuple:
+        '''
+        Take an action to the env.
+        '''
+        pass
