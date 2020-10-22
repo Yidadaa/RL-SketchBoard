78f8fc0c7d029c0dc8e92fc25ec4c39f111f89e3
Yifei Zhang <zhayife@amazon.com>
2020-10-22 21:55:40 +0800

    feat(code): update sketch board defination

--- env/SketchBoard.py
+++ env/SketchBoard.py
@@ -1,14 +1,30 @@
-
-
 class SketchBoardEnv(object):
-    def __init__(self):
+    def __init__(self, width: int, height: int, action_dim: int):
         '''
         Constructor of SketchBoard.
         '''
-        pass
+        self.action_space = None
+        self.observation_space = None
+        self.reward_range = None
+        self.sketch_board = None # np.array
 
     def step(action: list) -> tuple:
         '''
         Take an action to the env.
         '''
-        pass
+        raise NotImplementedError
+
+    def render():
+        '''
+        Render and return sketch board content.
+        '''
+        return self.sketch_board
+
+    def close():
+        '''
+        Close environment and clean up.
+        '''
+        raise NotImplementedError
+
+    def seed():
+        raise NotImplementedError
